b8454464740a859e0e86a0ddcad1a196ac873f6e
Tommaso Stentella <stentell@molgen.mpg.de>
2025-08-19 13:20:04 +0200

    update test

--- test/runtests.jl
+++ test/runtests.jl
@@ -84,12 +84,12 @@ end
         h = adapt_histogram(ibs_segments)
         @test all(h.weights .> 0)
         Ltot = sum(ibs_segments)
-        fits = pre_fit(h, 8, mu, Ltot; force = true)
+        fits = pre_fit(h, 8, mu, Ltot; force = true, require_convergence = false)
         nepochs = findlast(i->isassigned(fits, i), eachindex(fits))
         bestll = argmax(i->fits[i].lp, 1:nepochs)
         residuals = compute_residuals(h, mu, get_para(fits[bestll]))
-        @test abs(mean(residuals)) < 3/sqrt(200)
-        @test abs(std(residuals) - 1) < 3/sqrt(200)
+        @test abs(mean(residuals)) < 3/sqrt(length(residuals))
+        @test abs(std(residuals) - 1) < 3/sqrt(length(residuals))
     end
 
     # @testset "fit $(length(TN)÷2) epochs,  mu $mu, rho $rho" for (mu,rho,TN) in itr
