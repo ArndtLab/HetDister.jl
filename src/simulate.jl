c640e8d010bb557fd48c25d5a850e011e289c394
Peter Arndt <arndt@molgen.mpg.de>
2025-10-08 22:01:19 +0200

    refactor: update get_sim function to use Demography and Genome for ancestry simulation

--- src/simulate.jl
+++ src/simulate.jl
@@ -13,12 +13,17 @@ the IBS segments in the `h`.
 function get_sim!(params::Vector, h::Histogram, mu::Float64, rho::Float64; factor = 1)
 
     tnv = map(x -> ceil(Int, x), params)
-    pop = VaryingPopulation(; TNvector = tnv, mutation_rate = mu, recombination_rate = rho)
 
+    d = Demography()
+    PopSim.set_via_TNvector!(d, tnv)
+    g = Genome(UniformRate(rho), UniformRate(mu),  tnv[1])
 
+    anc = sim_ancestry(SMCprime(), d, g, 2)
+
+    
     h.weights .= 0
     @threads for _ in 1:factor
-        for ibs_segment in IBSIterator(PopSim.SMCprimeapprox.IBDIterator(pop), mu)
+        for ibs_segment in ibs(anc)
             push!(h, length(ibs_segment))
         end
     end
