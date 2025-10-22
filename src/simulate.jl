ab6ec024df4619cbe2e40f540ee77f220e449559
Tommaso Stentella <stentell@molgen.mpg.de>
2025-10-22 16:43:26 +0200

    integer number of simulated genomes

--- src/simulate.jl
+++ src/simulate.jl
@@ -23,7 +23,7 @@ function get_sim!(params::Vector, h::Histogram, mu::Float64, rho::Float64; facto
     
     h.weights .= 0
     hs = map(x->Histogram(h.edges), 1:factor)
-    @threads for i in 1:factor
+    @threads for i in 1:ceil(Int, factor)
         for ibs_segment in ibs(anc)
             push!(hs[i], length(ibs_segment))
         end
